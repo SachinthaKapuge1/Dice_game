53eefb5848254f275896af57c512599d188380ba
Sachintha Kapuge <k.sachintha@gtngroup.com>
2024-06-30 17:31:48 +0530

    input players with validation

--- index.py
+++ index.py
@@ -5,3 +5,16 @@ def roll():
     min_number=1    
     return random.randint(1,6)
 
+
+while True:
+    players = input("The number players(2,4) : ")
+    if players.isdigit():
+        players=int(players)
+        if 2 <= players <=4:
+            break
+        else:
+            print("Please Enter a value between 2 and 4")
+    else:
+        print("You are enterd a invalid input! Enter a valid input.")
+
+print(players)
